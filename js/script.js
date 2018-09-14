380288e7652d91f464c198e6582995747049b46c
BIEBER Baptiste <baptiste.bieber@accenture.com>
2018-09-14 18:02:45 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  // var list_objects = [];
+  // $('#QB_object_sel option').each(function() {
+  //   list_objects.append($(this).attr('value'));
+  // });
+  // console.log(list_objects);
+  // alert('rouge');
+
+});
